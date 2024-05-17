d0c68c7fdbfffe002b4f2d61b33ad81075514978
luc-git-projects <luc.git.projects@hotmail.com>
2024-05-17 17:05:35 -0300

    primeiro commit

---
+++ exercicio5/src/main/java/com/luciano/exercicio5/Exercicio5.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.luciano.exercicio5;
+
+/**
+ *
+ * @author luciano
+ */
+public class Exercicio5 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
